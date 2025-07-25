7fe3276fb7f60621e9fa0a12e1210d91b847f36d
Antonio Lieto <antonio.lieto1999@gmail.com>
2025-07-25 09:28:38 +0200

    initial commit
    base di partenza con i vari package

---
+++ src/main/java/antoniolieto/U5_W2_D5_progetto/U5W2D5ProgettoApplication.java
@@ -0,0 +1,13 @@
+package antoniolieto.U5_W2_D5_progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W2D5ProgettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W2D5ProgettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/antoniolieto/U5_W2_D5_progetto/U5W2D5ProgettoApplicationTests.java
@@ -0,0 +1,13 @@
+package antoniolieto.U5_W2_D5_progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W2D5ProgettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
