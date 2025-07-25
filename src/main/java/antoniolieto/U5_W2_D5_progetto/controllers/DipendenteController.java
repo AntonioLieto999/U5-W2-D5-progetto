1027ebe06aca9865ada5c3d153266414942353d9
Antonio Lieto <antonio.lieto1999@gmail.com>
2025-07-25 16:59:51 +0200

    Update DipendenteController.java

--- src/main/java/antoniolieto/U5_W2_D5_progetto/controllers/DipendenteController.java
+++ src/main/java/antoniolieto/U5_W2_D5_progetto/controllers/DipendenteController.java
@@ -7,12 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.DigestException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/dipendenti")
-public class ViaggioController {
+public class DipendenteController {
     @Autowired
     private DipendenteService service;
 
