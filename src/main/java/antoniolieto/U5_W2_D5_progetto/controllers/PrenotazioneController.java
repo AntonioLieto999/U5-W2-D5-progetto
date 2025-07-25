964a6c2fa49f9c2e5aa0bff74c70228053e09e5a
Antonio Lieto <antonio.lieto1999@gmail.com>
2025-07-25 17:31:46 +0200

    Prenotazioni Controller

---
+++ src/main/java/antoniolieto/U5_W2_D5_progetto/controllers/PrenotazioneController.java
@@ -0,0 +1,27 @@
+package antoniolieto.U5_W2_D5_progetto.controllers;
+
+import antoniolieto.U5_W2_D5_progetto.entities.Prenotazione;
+import antoniolieto.U5_W2_D5_progetto.payloads.PrenotazioneDTO;
+import antoniolieto.U5_W2_D5_progetto.services.PrenotazioneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/prenotazioni")
+public class PrenotazioneController {
+    @Autowired
+    private PrenotazioneService service;
+
+    @PostMapping
+    public ResponseEntity<Prenotazione> create(@RequestBody PrenotazioneDTO request){
+        return ResponseEntity.ok(service.creaPrenotazione(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Prenotazione>> getAll(){
+        return ResponseEntity.ok(service.getAll());
+    }
+}
